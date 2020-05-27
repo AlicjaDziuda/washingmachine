8b1a28a9317b7e734d9451136adec531f84ac9d5
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-05-27 13:53:22 +0200

    refactor tests

--- src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
@@ -21,58 +21,73 @@ class WashingMachineTest {
     @Mock
     private WaterPump waterPump;
     private WashingMachine washingMashine;
-
+    private LaundryBatch laundryBatch;
+    private ProgramConfiguration programConfiguration;
+    private LaundryStatus expectedLaundryStatus;
     @BeforeEach
     void setUp() throws Exception {
         washingMashine = new WashingMachine(dirtDetector, engine, waterPump);
     }
+
+    private void createLaundryBatch(Material material, double overweight) {
+        laundryBatch =  LaundryBatch.builder().withMaterialType(material).withWeightKg(overweight).build();
+    }
+
+    private void createConfigurationProgram(Program program, boolean spin) {
+        programConfiguration = ProgramConfiguration.builder().withProgram(program).withSpin(spin).build();
+    }
+
+    private void generateExpectedResultStatus(Program program, Result result, ErrorCode errorCode) {
+        expectedLaundryStatus = LaundryStatus.builder().withRunnedProgram(program).withResult(result).withErrorCode(errorCode).build();
+    }
+
     //start - jedyna publiczna metoda pralki
     @Test void washingWithStaticProgramShouldFinishWithSuccess() {
-        LaundryBatch batch = LaundryBatch.builder().withMaterialType(Material.COTTON).withWeightKg(7).build();
-        ProgramConfiguration program = ProgramConfiguration.builder().withProgram(Program.SHORT).withSpin(true).build();
-        LaundryStatus status =  washingMashine.start(batch,program);
-        assertTrue(LaundryStatus.builder().withRunnedProgram(Program.SHORT)
-                                .withResult(Result.SUCCESS).withErrorCode(ErrorCode.NO_ERROR).build().equals(status));
+        createLaundryBatch(Material.COTTON, 7d);
+        createConfigurationProgram(Program.SHORT, true);
+        LaundryStatus status =  washingMashine.start(laundryBatch,programConfiguration);
+        generateExpectedResultStatus(Program.SHORT, Result.SUCCESS, ErrorCode.NO_ERROR);
+        assertEquals(expectedLaundryStatus, status);
     }
 
     @Test void washingWithStaticProgramShouldCallEngineAndPump() throws WaterPumpException, EngineException {
-        LaundryBatch batch = LaundryBatch.builder().withMaterialType(Material.COTTON).withWeightKg(7).build();
-        ProgramConfiguration program = ProgramConfiguration.builder().withProgram(Program.SHORT).withSpin(true).build();
-        washingMashine.start(batch,program);
+        createLaundryBatch(Material.COTTON, 7d);
+        createConfigurationProgram(Program.SHORT, true);
+        washingMashine.start(laundryBatch,programConfiguration);
         InOrder order = Mockito.inOrder(waterPump,engine);
         order.verify(waterPump).pour(7d);
         order.verify(engine).runWashing(Program.SHORT.getTimeInMinutes());
         order.verify(waterPump).release();
         order.verify(engine).spin();
-
     }
 
     @Test void washingWithTooHeavyLaundryBatchShouldFinishedWithFailure() {
-        LaundryBatch batch = LaundryBatch.builder().withMaterialType(Material.COTTON).withWeightKg(21d).build();
-        ProgramConfiguration program = ProgramConfiguration.builder().withProgram(Program.SHORT).withSpin(false).build();
-        LaundryStatus status =  washingMashine.start(batch,program);
-        assertTrue(LaundryStatus.builder().withRunnedProgram(null)
-                                .withResult(Result.FAILURE).withErrorCode(ErrorCode.TOO_HEAVY).build().equals(status));
+        createLaundryBatch(Material.COTTON, 21d);
+        createConfigurationProgram(Program.SHORT, false);
+        LaundryStatus status =  washingMashine.start(laundryBatch,programConfiguration);
+        generateExpectedResultStatus(null, Result.FAILURE, ErrorCode.TOO_HEAVY);
+        assertEquals(expectedLaundryStatus,status);
+        //assertTrue(expectedLaundryStatus.equals(status));
     }
 
     @Test void washingWithAutodetectionShouldFinishedWithSuccess() {
-        LaundryBatch batch = LaundryBatch.builder().withMaterialType(Material.DELICATE).withWeightKg(2d).build();
-        ProgramConfiguration program = ProgramConfiguration.builder().withProgram(Program.AUTODETECT).withSpin(false).build();
+        createLaundryBatch(Material.DELICATE, 2d);
+        createConfigurationProgram(Program.AUTODETECT, false);
         //okreslenie zachowania zewnetrznej zaleznosci
-        when(dirtDetector.detectDirtDegree(batch)).thenReturn(new Percentage(40.0d));
-        LaundryStatus status =  washingMashine.start(batch,program);
-        assertTrue(LaundryStatus.builder().withRunnedProgram(Program.MEDIUM)
-                                .withResult(Result.SUCCESS).withErrorCode(ErrorCode.NO_ERROR).build().equals(status));
+        when(dirtDetector.detectDirtDegree(laundryBatch)).thenReturn(new Percentage(40.0d));
+        LaundryStatus status =  washingMashine.start(laundryBatch,programConfiguration);
+        generateExpectedResultStatus(Program.MEDIUM, Result.SUCCESS, ErrorCode.NO_ERROR);
+        assertEquals(expectedLaundryStatus,status);
     }
 
     @Test void washingWithAutodetectionShouldCallEngineWaterPumpAndDirtDetector() throws WaterPumpException, EngineException {
-        LaundryBatch batch = LaundryBatch.builder().withMaterialType(Material.DELICATE).withWeightKg(2d).build();
-        ProgramConfiguration program = ProgramConfiguration.builder().withProgram(Program.AUTODETECT).withSpin(false).build();
+        createLaundryBatch(Material.DELICATE, 2d);
+        createConfigurationProgram(Program.AUTODETECT, false);
         //okreslenie zachowania zewnetrznej zaleznosci
-        when(dirtDetector.detectDirtDegree(batch)).thenReturn(new Percentage(60.0d));
-        washingMashine.start(batch,program);
+        when(dirtDetector.detectDirtDegree(laundryBatch)).thenReturn(new Percentage(60.0d));
+        washingMashine.start(laundryBatch,programConfiguration);
         InOrder order = Mockito.inOrder(dirtDetector,waterPump,engine);
-        order.verify(dirtDetector).detectDirtDegree(batch);
+        order.verify(dirtDetector).detectDirtDegree(laundryBatch);
         order.verify(waterPump).pour(2d);
         order.verify(engine).runWashing(Program.LONG.getTimeInMinutes());
         order.verify(waterPump).release();
@@ -91,34 +106,34 @@ class WashingMachineTest {
     }*/
 
     @Test void washingWithIllegalArgEx_shouldFinishedWithFailure() {
-        LaundryBatch batch = LaundryBatch.builder().withMaterialType(Material.DELICATE).withWeightKg(2d).build();
-        ProgramConfiguration program = ProgramConfiguration.builder().withProgram(Program.AUTODETECT).withSpin(false).build();
+        createLaundryBatch(Material.DELICATE, 2d);
+        createConfigurationProgram(Program.AUTODETECT, false);
         //okreslenie zachowania zewnetrznej zaleznosci
-        doThrow(new IllegalArgumentException()).when(dirtDetector).detectDirtDegree(batch);
-        LaundryStatus status =  washingMashine.start(batch,program);
-        assertTrue(LaundryStatus.builder().withRunnedProgram(null)
-                                 .withResult(Result.FAILURE).withErrorCode(ErrorCode.UNKNOWN_ERROR).build().equals(status));
+        doThrow(new IllegalArgumentException()).when(dirtDetector).detectDirtDegree(laundryBatch);
+        LaundryStatus status =  washingMashine.start(laundryBatch,programConfiguration);
+        generateExpectedResultStatus(null, Result.FAILURE, ErrorCode.UNKNOWN_ERROR);
+        assertEquals(expectedLaundryStatus,status);
     }
 
     @Test void washingWithIllegalArgEx_shouldCallDirtDetectorOneTime() {
-        LaundryBatch batch = LaundryBatch.builder().withMaterialType(Material.DELICATE).withWeightKg(2d).build();
-        ProgramConfiguration program = ProgramConfiguration.builder().withProgram(Program.AUTODETECT).withSpin(false).build();
+        createLaundryBatch(Material.DELICATE, 2d);
+        createConfigurationProgram(Program.AUTODETECT, false);
         //okreslenie zachowania zewnetrznej zaleznosci
-        doThrow(new IllegalArgumentException()).when(dirtDetector).detectDirtDegree(batch);
-        washingMashine.start(batch,program);
-        verify(dirtDetector,times(1)).detectDirtDegree(batch);
+        doThrow(new IllegalArgumentException()).when(dirtDetector).detectDirtDegree(laundryBatch);
+        washingMashine.start(laundryBatch,programConfiguration);
+        verify(dirtDetector,times(1)).detectDirtDegree(laundryBatch);
     }
 
 
     @Test void washingWithEngineProblem_shouldFinishedWithEngineFailure() throws EngineException {
-        LaundryBatch batch = LaundryBatch.builder().withMaterialType(Material.DELICATE).withWeightKg(2d).build();
-        ProgramConfiguration program = ProgramConfiguration.builder().withProgram(Program.AUTODETECT).withSpin(true).build();
+        createLaundryBatch(Material.DELICATE, 2d);
+        createConfigurationProgram(Program.AUTODETECT, true);
         //okreslenie zachowania zewnetrznej zaleznosci
         //silnik sie psuje przy wirowaniu -> rzucanie wyjatkow przez metode ktora zwraca void!!!!
         doThrow(new EngineException()).when(engine).spin();
-        when(dirtDetector.detectDirtDegree(batch)).thenReturn(new Percentage(42.0d));
-        LaundryStatus status =  washingMashine.start(batch,program);
-        assertTrue(LaundryStatus.builder().withRunnedProgram(Program.MEDIUM)
-                                .withResult(Result.FAILURE).withErrorCode(ErrorCode.ENGINE_FAILURE).build().equals(status));
+        when(dirtDetector.detectDirtDegree(laundryBatch)).thenReturn(new Percentage(42.0d));
+        LaundryStatus status =  washingMashine.start(laundryBatch,programConfiguration);
+        generateExpectedResultStatus(Program.MEDIUM, Result.FAILURE, ErrorCode.ENGINE_FAILURE);
+        assertEquals(expectedLaundryStatus,status);
     }
 }
